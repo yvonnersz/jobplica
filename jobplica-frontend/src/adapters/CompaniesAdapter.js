f20c8ee55c236690251d06873190bcf8259d4cf2
yvonnersz <yvonnersz@gmail.com>
2020-08-13 10:36:36 -0700

    add files to components folder

---
+++ jobplica-frontend/src/adapters/CompaniesAdapter.js
@@ -0,0 +1,11 @@
+// This adapter will talk to our backend API.
+
+class Company {
+    constructor() {
+        this.baseUrl = 'http://localhost:3000/api/v1/companies'
+    }
+
+    getCompanies() {
+        return fetch(this.baseUrl).then(resp => resp.json())
+    }
+}
