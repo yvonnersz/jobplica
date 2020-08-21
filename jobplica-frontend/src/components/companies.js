f696f4fe06d32aa7f2645a3a3eed28f0e0dd35f3
yvonnersz <yvonnersz@gmail.com>
2020-08-20 21:08:11 -0700

    remove chart.js gem from frontend folder

--- jobplica-frontend/src/components/companies.js
+++ jobplica-frontend/src/components/companies.js
@@ -363,5 +363,29 @@ class Companies {
 
         let tableData = document.querySelector('tbody')
         tableData.appendChild(tr)
+
+        //donut chart
+
+        let myDoughnutChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: data,
+            options: options
+        });
+
+        data = {
+            datasets: [{
+                data: [10, 20, 30]
+            }],
+        
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: [
+                'Red',
+                'Yellow',
+                'Blue'
+            ]
+        };
+
+        let nav = document.querySelector('#container')
+        nav.appendChild(myDoughnutChart)
     }
 }
