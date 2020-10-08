e612f0c558778ed73745ca3d05077643ec0e8115
Yvonne <yvonnersz@gmail.com>
2020-10-07 21:21:11 -0700

    add metaheaders for fetch requests

--- jobplica-frontend/src/adapters/CompaniesAdapter.js
+++ jobplica-frontend/src/adapters/CompaniesAdapter.js
@@ -7,25 +7,23 @@ class CompaniesAdapter {
         return fetch(this.baseUrl).then(resp => resp.json())
     }
 
-    createCompany(companyObject) {
-        const company = companyObject
-
+    createCompany(company) {
         return fetch(this.baseUrl, {
             method: 'POST',
             headers: {
-                'content-type': 'application/json',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             },
             body: JSON.stringify({company}),
         }).then(resp => resp.json())
     }
 
-    updateCompany(newCompanyObject, id) {
-        const company = newCompanyObject
-
+    updateCompany(company, id) {
         return fetch(`${this.baseUrl}/${id}`, {
             method: 'PATCH',
             headers: {
-                'content-type': 'application/json',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             },
             body: JSON.stringify({company}),
         }).then(resp => resp.json())
@@ -35,7 +33,8 @@ class CompaniesAdapter {
         return fetch(`${this.baseUrl}/${id}`, {
             method: 'DELETE',
             headers: {
-                'content-type': 'application/json',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             }
         }).then(resp => resp.json())
     }
