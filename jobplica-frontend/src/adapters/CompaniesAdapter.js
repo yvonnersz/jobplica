650b58e25d55c86bdd5fea7f363f900f217f8756
Yvonne <yvonnersz@gmail.com>
2020-10-07 21:16:33 -0700

    cleaning up companies adapter

--- jobplica-frontend/src/adapters/CompaniesAdapter.js
+++ jobplica-frontend/src/adapters/CompaniesAdapter.js
@@ -1,7 +1,3 @@
-// This adapter will talk to our backend API.
-// Communicates to the API and relays that information to the frontend.
-// The "middleman".
-
 class CompaniesAdapter {
     constructor() {
         this.baseUrl = 'http://localhost:3000/api/v1/companies'
